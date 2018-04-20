1eb21be6d13dd0ca45bce2e547cba4e41b60d5e9
Sergey <yolorusher@gmail.com>
2018-04-20 13:14:09 +0300

    1.0.10 minor test fixes

--- Shaykhullin.Serializer/Shaykhullin.Serializer.Tests/ScopeTests.cs
+++ Shaykhullin.Serializer/Shaykhullin.Serializer.Tests/ScopeTests.cs
@@ -35,6 +35,8 @@ namespace Shaykhullin.Serializer.Tests
 						var result = serializer.Deserialize<Base>(stream);
 						
 						Assert.IsType<Base>(result);
+						
+						Assert.Equal(10, result.Id);
 					}
 				}
 				
